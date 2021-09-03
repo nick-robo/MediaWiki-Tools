61ec6c351e024cc342df30ab13dda79843370117
Nick <nickrrrr@gmail.com>
2021-09-03 12:37:15 +0200

    added api search

--- app/classes.py
+++ app/classes.py
@@ -5,11 +5,13 @@ from urllib.parse import urlparse
 import re
 from typing import Callable, Union
 from itertools import combinations
+import mwparserfromhell as mwp
+from mediawiki import MediaWiki 
 
 
 class WikiSubsetter:
     def __init__(self, input_url: str):
-        """Create WikiSubsetter object.
+        """Create WikiSubsetter object from a MediaWiki page.
 
         Args:
             input_url (str): A url from the wiki to be subsetted.
@@ -27,6 +29,11 @@ class WikiSubsetter:
         if not len(self.parsed_url.netloc.split(".")) > 1:
             raise ValueError(f'Invalid url: {input_url}')
 
+        # get base url 
+        self.base_url = self.parsed_url._replace(
+            path='', params='', fragment=''
+        ).geturl()
+
         # get page name (wiki.com/page_name/Page) from input
         match = None
         if self.parsed_url.path:
@@ -34,9 +41,6 @@ class WikiSubsetter:
             match = re.match('%(.*?)%', path)
 
         self.page_name = match.group(1) if match else None
-        self.base_url = self.parsed_url._replace(
-            path='', params='', fragment=''
-        ).geturl()
 
         page = requests.get(self.base_url)
         if not page.ok:
@@ -63,11 +67,37 @@ class WikiSubsetter:
 
         self.page_base_url = self.base_url + '/' + self.page_name + '/'
 
-    def get_data(self, input) -> BeautifulSoup:
+        # try to get api
+
+        # build target list
+        # TODO: Research more targets
+        targets = [
+            self.base_url + '/wiki/api.php',
+            self.base_url + '/w/api.php',
+            self.page_base_url + 'api.php',
+        ]
+
+        for target in targets:
+            try:
+                self.mw = MediaWiki(target)
+                self.api_url = target
+                self.has_api = True
+                break
+            except Exception:
+                continue
+        else:
+            self.mw = None
+            self.has_api = False
+            self.api_url = None
+        
+
+    def get_data(self, input: str) -> BeautifulSoup:
         # TODO: deal with url or category name
         if 'http' in input:
             page = requests.get(input)
         else:
+            # get fragment if link
+            input = input.split('/')[-1]
             if 'List' in input:
                 url = self.page_base_url + input
             else:
@@ -96,6 +126,7 @@ class WikiSubsetter:
             subcategories. Defaults to False.
 
             list_only (bool, optional): Only get links that are lists.
+            Assumption (slightly risky): Lists are on the first page.
             Defaults to False.
 
         Returns:
@@ -170,11 +201,13 @@ class WikiSubsetter:
         operator = 'update' if operation == 'union' else 'intersection_update'
 
         # use lists to increase efficiency
+        # caveat: slightly slower if list does not exist
         if use_lists and operation == 'intersection' and len(categories) > 1:
             combs = combinations(categories, 2)
+            done = set()
             for comb in combs:
                 # skip if both categories have been intersected
-                if all([x not in categories for x in comb]):
+                if all([x in done for x in comb]):
                     continue
                 lists = self.get_pages(comb[0], list_only=True)
                 # replace underscore and remove plural ([:-1])
@@ -187,17 +220,15 @@ class WikiSubsetter:
                 if lists:
                     # if list is not empty get links from the first one
                     # (should always be one result)
-                    list_link = lists[0].split('/')[-1]
-                    list_pages = self.get_pages(list_link)
+                    list_pages = self.get_pages(lists[0])
                     # if page_list is not empty, intersect
                     if pages_list:
                         pages_list = set(pages_list).intersection(list_pages)
                     else:
                         pages_list.extend(list_pages)
                     # remove list items from categories
-                    categories.remove(comb[0])
-                    categories.remove(comb[1])
-            if not categories:
+                    done.update(comb)
+            if len(done) == len(set(categories)):
                 return list(pages_list)
 
         # edge cases
@@ -215,19 +246,28 @@ class WikiSubsetter:
                 getattr(page_set, operator)(
                     self.get_pages(category, get_subcats))
 
-        if pages_list is not None:
+        if pages_list:
             getattr(page_set, operator)(pages_list)
 
         return list(page_set)
+
+    def get_info(pages: Union[list[str], str]):
+
+        parsed_wikitext = mwp.parse(wikitext)
+        # get params dict from parsed template
+        p_dict = {(kv := p.split('=', 1))[
+            0].strip(): kv[1].strip() for p in biobox.params}
+
 # %%
 
 
 ws = WikiSubsetter('www.***REMOVED***pedia.com')
-cats = ['Amazons', 'Italian']
-result_list = ws.get_set(cats, 'intersection', use_lists=True)
+cats = ['Caucasian', 'Asian']
+# result_list = ws.get_set(cats, 'intersection',
+#  use_lists=True)
 result_naive = ws.get_set(cats, 'intersection')
 
-# %%
-assert not set(result_naive) ^ set(result_list)
+# assert not set(result_naive) ^ set(result_list)
+print('Done')
 
 # %%
