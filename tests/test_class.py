923b890a37c9f1e581b6e3eb1c7cd57021d79754
Nick <nickrrrr@gmail.com>
2021-09-07 09:50:49 +0200

    use deepdiff for tests

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,4 +1,4 @@
-from app.classes import WikiSubsetter
+from app.classes import MediaWikiTools
 import pytest
 from deepdiff import DeepDiff
 
@@ -11,13 +11,15 @@ wiki_list_no_api = ('https://proteopedia.org', 'https://www.werelate.org/')
 
 
 @pytest.fixture()
-def check_pagelist_equal(reslist1: list[str], reslist2: list[str]) -> None:
-	pass
+def check_pagelist_equivalent(reslist1: list[str], reslist2: list[str]) -> None:
+	# TODO: Flexibly check whether get pages results are equivalent 
+	# 			up to deletions and recent changes
+	raise NotImplementedError()
 
 
 @pytest.mark.parametrize('page', wiki_list)
 def test_class_init_api(page):
-	ws = WikiSubsetter(page)
+	ws = MediaWikiTools(page)
 
 	assert ws.has_api
 	assert ws.mw
@@ -26,7 +28,7 @@ def test_class_init_api(page):
 
 @pytest.mark.parametrize('page', wiki_list_no_api)
 def test_class_init_no_api(page):
-	ws = WikiSubsetter(page)
+	ws = MediaWikiTools(page)
 
 	assert not ws.has_api
 	assert not ws.mw
@@ -34,7 +36,7 @@ def test_class_init_no_api(page):
 
 
 def test_class_get_pages():
-	ws = WikiSubsetter('https://en.uncyclopedia.co')
+	ws = MediaWikiTools('https://en.uncyclopedia.co')
 	cats = [
 	    'Your Mom',
 	    'Your_Mom',
@@ -46,18 +48,26 @@ def test_class_get_pages():
 	res_no_api = ws.get_pages(cats[0], use_api=False)
 
 	assert len(res_api) != 0
-	assert len(set(res_api) ^ set(res_no_api)) == 0
+	assert set(res_api) == set(res_no_api), \
+     f'API and non-API results differ: {DeepDiff(res_api, res_no_api, ignore_order=True)}'
 
-	assert len(set(res_api) ^ set(ws.get_pages(cats[1]))) == 0
+	res = ws.get_pages(cats[1])
+	assert set(res_api) == set(res), \
+     f'API and non-API results differ: {DeepDiff(res_api, res_no_api, ignore_order=True)}'
 
-	assert len(set(res_api) ^ set(ws.get_pages(cats[2]))) == 0
+
+	res = ws.get_pages(cats[2])
+	assert set(res_api) == set(res), \
+     f'API and non-API results differ: {DeepDiff(res_api, res_no_api, ignore_order=True)}'
+
 
 	# test get_subcats
 	res_api = ws.get_pages(cats[0], get_subcats=True)
 	res_no_api = ws.get_pages(cats[0], get_subcats=True, use_api=False)
 
 	assert res_api
-	assert len(set(res_api) ^ set(res_no_api)) == 0
+	assert set(res_api) == set(res_no_api), \
+     f'API and non-API results differ: {DeepDiff(res_api, res_no_api, ignore_order=True)}'
 
 	# test recursive
 	res_api = ws.get_pages(cats[0],
@@ -72,9 +82,8 @@ def test_class_get_pages():
 	                          with_subcats=True,
 	                          use_api=False)
 
-	diff = DeepDiff(res_api,
-	                res_no_api,
-	                ignore_order=True)
+	diff = DeepDiff(res_api, res_no_api, ignore_order=True)
 
+	# check missing or added categories
 	assert not diff.get('dictionary_item_removed')
 	assert not diff.get('dictionary_item_added')
