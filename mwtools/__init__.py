6976e9b5103443b87823e06adc2ae78eccefddd1
Nick <nickrrrr@gmail.com>
2023-09-06 16:07:12 +1000

    move to pdm

--- mwtools/__init__.py
+++ mwtools/__init__.py
@@ -149,9 +149,4 @@ wiki.get_set(['Countries in Asia',
 """
 
 from .mediawikitools import MediaWikiTools
-
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass  # package is not installed
+__all__ = ["MediaWikiTools"]
